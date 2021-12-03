ef3aab3a6f52634a5280668b64356e9b56c4d649
”Ivan <vaheosipyan0@gmail.com>
2021-12-03 06:22:54 +0400

    feat:Task ready

--- InsertDigit7.js
+++ InsertDigit7.js
@@ -1,1 +1,13 @@
+let num = 3545.6449;
 
+while (Math.floor(num) != num) num *= 10;
+let dig = !num,
+  b = 0;
+while (num) {
+  (c = 1 << num % 10), (dig & c && (b |= c)) || (dig |= c);
+  num = Math.floor(num / 10);
+}
+
+for (i = 0; i < 10; i++)
+  if (dig & (1 << i) & b)
+    console.log("Number " + i + " repeated several times.");
