930a6204435e66d33354bc6913dd36d8c0970947
”Ivan <vaheosipyan0@gmail.com>
2021-12-03 06:27:48 +0400

    feat:Task ready

--- reverseNumber8.js
+++ reverseNumber8.js
@@ -1,1 +1,15 @@
+let num = 736;
+let num1;
+let sum = 0;
+let z = num;
 
+while (num > 0) {
+  num1 = num % 10;
+  sum = sum * 10 + num1;
+  num = parseInt(num / 10);
+}
+console.log(sum);
+let count = num / 10;
+sum = Math.trunc(count);
+
+console.log(sum);
