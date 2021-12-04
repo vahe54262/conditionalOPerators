6a73695c2b9d2f5a0a23f48c0f98035228a6f3fc
”Ivan <vaheosipyan0@gmail.com>
2021-12-04 23:29:07 +0400

    fix:cod is correct

--- quotRem.js
+++ quotRem.js
@@ -1,19 +1,23 @@
-"use strict";
-
+'use strict';
 let mul = 1;
 let quot = 0;
 let rem = 0;
 let num = 5678;
 let sum = 0;
-while (num > 10) {
-  let count = num % 10;
-  num = (num - count) / 10;
-  sum += count;
-  mul *= count;
-  if (num < 10) {
-    sum += num;
-    mul *= num;
+let count = 0;
+if (num !== 0) {
+  while (num > 10) {
+    count = num % 10;
+    num = (num - count) / 10;
+    sum += count;
+    mul *= count;
+    if (num < 10) {
+      sum += num;
+      mul *= num;
+    }
   }
+} else {
+  console.log("can note");
 }
 
 if (mul % sum !== 0) {
